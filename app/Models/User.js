b8c193a41e5cdd08aa59cdc0729d50305ee5cb9b
Maurice <robloxfilmcam@gmail.com>
2020-05-02 08:53:34 +0200

    Add disabling for Timestamps

--- app/Models/User.js
+++ app/Models/User.js
@@ -9,6 +9,7 @@ const Model = use('Model')
 class User extends Model {
   static boot () {
     super.boot()
+    this.addTrait('NoTimestamp')
 
     /**
      * A hook to hash the user password before saving
